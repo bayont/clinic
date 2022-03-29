feb3e9875936d99aff59893cfa14843dd55b4d43
bayont <bayontmen@gmail.com>
2022-03-29 13:05:58 -0400

    feat: footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Footer } from "../components/Footer";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <div className="min">
+        <div className="padding">
+          <Component {...pageProps} />
+        </div>
+        <Footer />
+      </div>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
